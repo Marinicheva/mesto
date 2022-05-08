ca7a2e2eb3ffc8b157fd9ce75e3a932e61e0bfe6
Ekaterina Marinicheva <marri.nich@yandex.ru>
2022-05-08 17:01:13 +0300

    fix: исправить ошибки в скрипте

--- scripts/index.js
+++ scripts/index.js
@@ -1,66 +1,40 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const editBtn = document.querySelector('.profile__edit-btn');
-    const profileName = document.querySelector('.profile__name');
-    const profileDescription = document.querySelector('.profile__description');
+const editBtn = document.querySelector('.profile__edit-btn');
+const profileName = document.querySelector('.profile__name');
+const profileDescription = document.querySelector('.profile__description');
 
-    const modal = document.querySelector('.modal');
-    const closeBtn = modal.querySelector('.modal__close');
-    const userName = modal.querySelector('.modal__input-name');
-    const userDescription = modal.querySelector('.modal__input-description');
-    const editForm = modal.querySelector('.modal__form');
+const modal = document.querySelector('.modal');
+const closeBtn = modal.querySelector('.modal__close');
+const userName = modal.querySelector('.modal__input-name');
+const userDescription = modal.querySelector('.modal__input-description');
+const editForm = modal.querySelector('.modal__form');
 
-    let currentUserName = profileName.textContent;
-    let currentUserDescription = profileDescription.textContent;
+let currentUserName = profileName.textContent;
+let currentUserDescription = profileDescription.textContent;
 
-    function openModal() {
-        userName.value = currentUserName;
-        userDescription.value = currentUserDescription;
-        modal.classList.add('modal_opened');
-    }
+function openModal() {
+    userName.value = currentUserName;
+    userDescription.value = currentUserDescription;
+    modal.classList.add('modal_opened');
+}
 
-    function modalClose() {
-        modal.classList.remove('modal_opened');
-    }
+function modalClose() {
+    modal.classList.remove('modal_opened');
+}
 
-    function editProfile() {
-        profileName.insertAdjacentText('afterbegin', userName.value);
-        profileDescription.insertAdjacentText('afterbegin', userDescription.value);
+function editProfile(evt) {
+    evt.preventDefault();
+    profileName.textContent = userName.value;
+    profileDescription.textContent = userDescription.value;
 
-        currentUserName = userName.value;
-        currentUserDescription = userDescription.value;
-    }
+    currentUserName = userName.value;
+    currentUserDescription = userDescription.value;
+    modalClose();
+}
 
-    function checkChanges() {
-        if (userName.value === currentUserName && userDescription.value === currentUserDescription) {
-            modalClose();
-        } else {
-            profileName.textContent = '';
-            profileDescription.textContent = '';
-            editProfile();
-            modalClose();
-        }
-    }
+/*Обработка событий для открытия и закрытия модального окна*/
+editBtn.addEventListener('click', openModal);
 
-    /*Обработка событий для открытия и закрытия модального окна*/
-    editBtn.addEventListener('click', event => {
-        event.preventDefault();
-        openModal();
-    });
+closeBtn.addEventListener('click', modalClose);
 
-    closeBtn.addEventListener('click', event => {
-        event.preventDefault();
-        modalClose();
-    });
-
-    document.addEventListener('keydown', (event) => {
-       if (modal.classList.contains('modal_opened') && event.code === 'Escape') {
-        modalClose();
-       }
-    });
-
-    /*Обработка события отправки формы*/
-    editForm.addEventListener('submit',  event => {
-        event.preventDefault();
-        checkChanges();
-    });
-});
+/*Обработка события отправки формы*/
+editForm.addEventListener('submit',  editProfile);
