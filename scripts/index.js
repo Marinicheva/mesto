c4f243557d74e7bd451f68f8dd21ee16dcfeeedb
Ekaterina Marinicheva <marri.nich@yandex.ru>
2022-05-19 13:01:26 +0300

    feat: добавить необходимый для работы код

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,30 @@
+const initialCards = [
+    {
+        name: 'Карачаевск',
+        link: './images/photo-1.jpg',
+    },
+    {
+        name: 'Гора Эльбрус',
+        link: './images/photo-2.jpg',
+    },
+    {
+        name: 'Домбай',
+        link: './images/photo-3.jpg',
+    },
+    {
+        name: 'Калуга',
+        link: './images/photo-4.jpg',
+    },
+    {
+        name: 'Санкт-Петербург',
+        link: './images/photo-5.jpg',
+    },
+    {
+        name: 'Калининград',
+        link: './images/photo-6.jpg',
+    },
+];
+
 const editBtn = document.querySelector('.profile__edit-btn');
 const profileName = document.querySelector('.profile__name');
 const profileDescription = document.querySelector('.profile__description');
