6fdc16dcc50f7b34081eb955c01a7b62d85b1819
Ekaterina Marinicheva <marri.nich@yandex.ru>
2022-05-19 14:19:26 +0300

    feat: реализовать функционал добавления карточек

--- scripts/index.js
+++ scripts/index.js
@@ -24,28 +24,35 @@ const initialCards = [
         link: './images/photo-6.jpg',
     },
 ];
+const cardsList = document.querySelector('.gallery__list');
 
 const editBtn = document.querySelector('.profile__edit-btn');
+const modalEdit = document.querySelector('.modal_type_edit-form');
+const editForm = modalEdit.querySelector('.modal__form_type-edit');
+const userName = modalEdit.querySelector('.modal__input-name');
+const userDescription = modalEdit.querySelector('.modal__input-description');
 const profileName = document.querySelector('.profile__name');
 const profileDescription = document.querySelector('.profile__description');
 
-const modal = document.querySelector('.modal');
-const closeBtn = modal.querySelector('.modal__close');
-const userName = modal.querySelector('.modal__input-name');
-const userDescription = modal.querySelector('.modal__input-description');
-const editForm = modal.querySelector('.modal__form');
+const addCardBtn = document.querySelector('.profile__add-btn');
+const modalAddCard = document.querySelector('.modal_type_add-new-card');
+const addCardForm = modalAddCard.querySelector('.modal__form_type-add-card');
+const inputPlaceName = addCardForm.querySelector('.modal__input-place-name');
+const inputPlaceLink = addCardForm.querySelector('.modal__input-place-link');
+
+const closeBtn = document.querySelectorAll('.modal__close');
 
 let currentUserName = profileName.textContent;
 let currentUserDescription = profileDescription.textContent;
 
-function openModal() {
+function openModal(popup) {
     userName.value = currentUserName;
     userDescription.value = currentUserDescription;
-    modal.classList.add('modal_opened');
+    popup.classList.add('modal_opened');
 }
 
-function closeModal() {
-    modal.classList.remove('modal_opened');
+function closeModal(popup) {
+    popup.classList.remove('modal_opened');
 }
 
 function editProfile(evt) {
@@ -55,13 +62,52 @@ function editProfile(evt) {
 
     currentUserName = userName.value;
     currentUserDescription = userDescription.value;
-    closeModal();
+    closeModal(modalEdit);
 }
 
-/*Обработка событий для открытия и закрытия модального окна*/
-editBtn.addEventListener('click', openModal);
+function createCard(name, link) {
+    const template = document.querySelector('.card-template').content;
+    const cardTemplate = template.querySelector('.gallery__item').cloneNode(true);
+    
+    cardTemplate.querySelector('.gallery__img').src = link;
+    cardTemplate.querySelector('.gallery__img').alt = `Пользовательское фото места ${name}`;
+    cardTemplate.querySelector('.gallery__img-caption').textContent = name;
 
-closeBtn.addEventListener('click', closeModal);
+    cardsList.prepend(cardTemplate);
+}
 
-/*Обработка события отправки формы*/
+/*Обработка событий для открытия модальных окон*/ 
+editBtn.addEventListener('click', () => openModal(modalEdit));
+addCardBtn.addEventListener('click', () => openModal(modalAddCard));
+
+/*Обработка событий для закрытия модальных окон*/
+closeBtn.forEach(item => {
+    const modalNeedBeClose = item.parentElement.parentElement;
+    item.addEventListener('click', () => {
+        closeModal(modalNeedBeClose);
+    });
+});
+
+/*Редактирование профиля*/
 editForm.addEventListener('submit',  editProfile);
+
+/*Создание новой карточки*/
+addCardForm.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+    const newPlace = inputPlaceName.value;
+    const newLink = inputPlaceLink.value;
+
+    createCard(newPlace, newLink);
+    inputPlaceName.value = '';
+    inputPlaceLink.value ='';
+
+    closeModal(modalAddCard);
+});
+
+/*Динамическое создание карточек*/
+initialCards.forEach(item => {
+    const cardName = item.name;
+    const cardLink = item.link;
+    
+    createCard(cardName, cardLink);
+});
