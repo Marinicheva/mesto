de48f93511283766781218040041ad8a8b93e6eb
Ekaterina Marinicheva <marri.nich@yandex.ru>
2022-07-08 23:03:22 +0300

    feat: add settings for building project

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: {
@@ -28,6 +29,13 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
         type: 'asset/resource'
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, {
+          loader: 'css-loader'
+        }]
+
       }
     ]
   },
@@ -38,5 +46,6 @@ module.exports = {
       scriptLoading: 'blocking'
     }),
     new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin()
   ]
 };
