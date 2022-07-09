73593f4fa006b532006fc878c3861eabd86bc933
Ekaterina Marinicheva <marri.nich@yandex.ru>
2022-07-09 18:46:33 +0300

    fix: find popupImg and popuCaption in constructor

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -3,15 +3,15 @@ import Popup from "./Popup.js";
 export default class PopupWithImage extends Popup {
   constructor(popupSelector) {
     super(popupSelector);
+    this._popupImg = this._popup.querySelector('.popup__img-fullscreen');
+    this._popupCaption = this._popup.querySelector('.popup__fullscreen-caption');
+
   }
 
   openPopup({
     name,
     link
   }) {
-    this._popupImg = this._popup.querySelector('.popup__img-fullscreen');
-    this._popupCaption = this._popup.querySelector('.popup__fullscreen-caption');
-
     this._popupImg.src = link;
     this._popupImg.alt = `Пользовательское фото места ${name}`;
     this._popupCaption.textContent = name;
